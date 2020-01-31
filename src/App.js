c4ec7ed055dbf51b3eecba0243587176dd6a0f65
HK <user@HK.local>
2020-01-31 17:32:01 +0900

    All you need to know about State

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import React from "react";
 
 class App extends React.Component {
   state = {
-    count: 0
+    count: 0,
+    doub: 2
   };
   add = () => {
-    console.log("add");
+    this.setState(current => ({
+      count: current.count + 1,
+      doub: current.doub + 2
+    }));
   };
   minus = () => {
-    console.log("minus");
+    this.setState(current => ({
+      count: current.count - 1,
+      doub: current.doub - 2
+    }));
   };
   render() {
     return (
       <div>
         <h1>The number is : {this.state.count}</h1>
+        <h1>The double number is :{this.state.doub}</h1>
         <button onClick={this.add}>Add</button>
         <button onClick={this.minus}>Minus</button>
       </div>
@@ -23,3 +31,5 @@ class App extends React.Component {
 }
 
 export default App;
+// https://mtops.hmc.co.kr/e.jsp?P=FmsU5P-ILWjqnvmp73fim I-blnJp38hZnTDqPJUDMLh L9GXCD5yv0A
+// https://mtops.hmc.co.kr/e.jsp?P=FmsU5P-ILWjqnvmp73fim NSs7Gont8ruH3X0HZyhACt L9GXCD5yv0A
